0cbfd100a00b71684f8ba1aa1a39059d5254b1aa
AryanChothe <2303052@ritindia.edu>
2026-06-28 20:31:27 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/upi_tracker/config/SecurityConfig.java
+++ src/main/java/com/example/upi_tracker/config/SecurityConfig.java
@@ -42,10 +42,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-       config.setAllowedOrigins(List.of(
-    "http://localhost:5173",
-    "https://YOUR-FRONTEND.onrender.com"
-));
+       config.setAllowedOriginPatterns(List.of(
+        "http://localhost:5173",
+        "https://refundify-frontend.onrender.com"
+    ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
 
